b4d4ae1a72c2d8e594d15c0e677cd99ffb83e878
cuiqin2012210831 <3126023230@qq.com>
2018-11-04 19:43:54 +0800

    er

---
+++ Test3.java
@@ -0,0 +1,25 @@
+package a_java入门.day01;
+/*java的基本数据类型*/
+public class Test3 {
+	public static void main(String[] args) {
+		boolean flag = true;
+		/*0000 0000
+		 *0000 0110 
+		 *1111 1111 +1=  10000 0000
+		 * */
+		flag = false;
+		//flag = 1;错误
+		/*000
+		 * 001
+		 * 010
+		 * 011
+		 * 100
+		 * 101
+		 * 110
+		 * 111
+		 * 
+		 * */
+		char c1 = 97;
+		System.out.println("c1="+c1);
+	}
+}
