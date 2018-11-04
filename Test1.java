2da5fdcb8c6aa7e4646e26151425ae0916f028c3
cuiqin2012210831 <3126023230@qq.com>
2018-11-04 19:17:27 +0800

    与

---
+++ Test1.java
@@ -0,0 +1,20 @@
+package a_java入门.day01;
+/*ctrl+F11可自动编译并运行
+ * 也可右键找到Run as->run as javaApplication
+ * 
+ * */
+public class Test1 {
+	/*这是多行注释，下面的main是程序的入口，一应用程序中只能有一个main*/
+	public static void main(String[] args) {
+		//单行注释
+		System.out.println("哇哈哈......");
+		//年龄为10岁
+		int age = 10;
+		/*age=+10*/
+		System.out.println("age1="+age);
+		age = 20;
+		System.out.println("age2="+age);
+		int score = 80;
+		System.out.println("score="+score);
+	}
+}
